3036593adfe44e9cf7b34324a2c443f6153e67a9
Lukas Jonsson <49129784+derverdox@users.noreply.github.com>
2026-06-07 00:51:01 +0200

    Create docusaurus.config.js

---
+++ docusaurus.config.js
@@ -0,0 +1,46 @@
+const config = {
+  title: 'Solar Miner App',
+  tagline: 'Stop wasting solar energy. Start mining smarter.',
+  url: 'https://docs.solarminer.app',
+  baseUrl: '/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
+  organizationName: 'derverdox',
+  projectName: 'solar-miner-docs',
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          routeBasePath: '/', // Lädt die Docs direkt auf der Hauptseite
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        blog: false, // Wir brauchen keinen Blog
+        theme: {
+          customCss: [], // Keine extra CSS-Datei nötig für den Start
+        },
+      },
+    ],
+  ],
+
+  themeConfig: {
+    colorMode: {
+      defaultMode: 'dark',
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: 'Solar Miner Docs',
+      items: [
+        {
+          href: 'https://solarminer.app',
+          label: 'Back to Website',
+          position: 'right',
+        },
+      ],
+    },
+  },
+};
+
+export default config;
